b4cce79eb999e86b07f217d220701b5f761dc8d5
Pradeep <preddy@kiwiup.com>
2014-01-12 01:55:00 +0530

    mainscreen initial layout with all elements, Needs better coloring and alignment

--- src/com/example/scorer/MainScreen.java
+++ src/com/example/scorer/MainScreen.java
@@ -111,7 +111,7 @@ public class MainScreen extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.add_player_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
