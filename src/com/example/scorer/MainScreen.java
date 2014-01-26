7aca26a43cfc680b9650a83a9b915cf378a67c1c
Pradeep <preddy@kiwiup.com>
2014-01-27 01:14:05 +0530

    handle name and score limit input and pass them to the list view

--- src/com/example/scorer/MainScreen.java
+++ src/com/example/scorer/MainScreen.java
@@ -9,6 +9,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -17,143 +23,76 @@ import android.view.View;
  * @see SystemUiHider
  */
 public class MainScreen extends Activity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
-
+	
+	private static int scoreLimit;
+	private static ArrayAdapter<String> playerListAdaptar;
+	Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+ 
         setContentView(R.layout.activity_main_screen);
+        toast = Toast.makeText(this, "Invalid Score Limit", Toast.LENGTH_LONG);
+        
+        playerListAdaptar = new ArrayAdapter<String>(this,
+    	        android.R.layout.simple_list_item_1);
+        
+        ListView playerList = (ListView) findViewById(R.id.list_of_players_view);
+    	playerList.setAdapter(playerListAdaptar);
 
-        final View controlsView = findViewById(R.id.fullscreen_content_controls);
-        final View contentView = findViewById(R.id.fullscreen_content);
-
-        // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
-        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
-        mSystemUiHider.setup();
-        mSystemUiHider
-                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-                    // Cached values.
-                    int mControlsHeight;
-                    int mShortAnimTime;
-
-                    @Override
-                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-                    public void onVisibilityChange(boolean visible) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.
-                            if (mControlsHeight == 0) {
-                                mControlsHeight = controlsView.getHeight();
-                            }
-                            if (mShortAnimTime == 0) {
-                                mShortAnimTime = getResources().getInteger(
-                                        android.R.integer.config_shortAnimTime);
-                            }
-                            controlsView.animate()
-                                    .translationY(visible ? 0 : mControlsHeight)
-                                    .setDuration(mShortAnimTime);
-                        } else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
-                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-                        }
-
-                        if (visible && AUTO_HIDE) {
-                            // Schedule a hide().
-                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                        }
-                    }
-                });
-
-        // Set up the user interaction to manually show or hide the system UI.
-        contentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (TOGGLE_ON_CLICK) {
-                    mSystemUiHider.toggle();
-                } else {
-                    mSystemUiHider.show();
-                }
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.add_player_button).setOnTouchListener(mDelayHideTouchListener);
+    }
+    
+    /** Called when the user touches the start button */
+    public void startGame(View view) {
+    	//verify non null score
+    	EditText scoreLimitEditor = (EditText) findViewById(R.id.score_limit_editor);
+    	try{
+    		scoreLimit = Integer.parseInt(scoreLimitEditor.getText().toString());
+    	}
+    	catch (NumberFormatException e)
+    	{
+    		toast = Toast.makeText(this, "Invalid Score Limit", Toast.LENGTH_LONG);
+    		toast.show();
+    		return;
+    	}
+    	Toast toast = Toast.makeText(this, "Score Limit "+scoreLimit, Toast.LENGTH_LONG);
+		toast.show();
+		
+		if(playerListAdaptar.getCount() < 2)
+		{
+			toast.cancel();
+			toast = Toast.makeText(this, "Add Atleast 2 players", Toast.LENGTH_LONG);
+			toast.show();
+			return;
+		}
+		else
+		{
+			toast.cancel();
+			toast = Toast.makeText(this, playerListAdaptar.getCount() + " players present", Toast.LENGTH_LONG);
+			toast.show();
+		}
+    }
+    
+    /** Called when the user touches the start button */
+    public void addPlayer(View view) {
+    	//verify non null score
+    	EditText playerNameEditor = (EditText) findViewById(R.id.user_name_editor);
+    	
+    	String playerName = playerNameEditor.getText().toString();
+    	playerNameEditor.setText("");
+    	if(playerName == null || playerName.trim().isEmpty())
+    	{
+    		toast.cancel();
+    		toast = Toast.makeText(this, "Enter Valid Player Name", Toast.LENGTH_LONG);
+    		toast.show();
+    		return;
+    	}
+ 
+    	playerListAdaptar.add(playerName);
+    	
+    	toast.cancel();
+    	toast = Toast.makeText(this, "Player "+playerName+ " added", Toast.LENGTH_LONG);
+		toast.show();
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
-
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
-
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSystemUiHider.hide();
-        }
-    };
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
 }
